a2040ab5d8b8f4023d15527284a5f721fb779f27
McHaillet <martenchaillet@gmail.com>
2025-03-17 15:10:46 +0100

    update word map printing

--- datasets/check_titles.py
+++ datasets/check_titles.py
@@ -8,14 +8,6 @@ with open("random_emdb_entries.json") as f:
 
 titles = [x["admin"]["title"] for x in d]
 
-ribo = [t for t in titles if "ribo" in t.lower()]
-
-print(f"Random EMDB subset contains {len(ribo)} structures with ribo in the title.")
-
-sars = [t for t in titles if "sars" in t.lower()]
-
-print(f"Random EMDB subset contains {len(sars)} structures with sars in the title.")
-
 
 def count_word_occurrences(text):
     # Convert text to lowercase
@@ -38,8 +30,5 @@ text = " ".join([t.strip(".") for t in titles])
 word_map = count_word_occurrences(text)
 
 print(word_map)
-# Output: Counter({'text': 2, 'this': 2, 'sample': 2, 'words': 2, 'is': 1, 'a': 1, 'contains': 1, 'some': 1, 'that': 1, 'repeat': 1, 'occur': 1, 'multiple': 1, 'times': 1})
-
-# To get the most common words
-print(word_map.most_common(3))
-# Output: [('text', 2), ('this', 2), ('sample', 2)]
+print(f"Ribosome has {word_map['ribosome']} occurences in the EMDB entry titles.")
+print(f"Sarscov2 has {word_map['sarscov2']} occurences in the EMDB entry titles.")
