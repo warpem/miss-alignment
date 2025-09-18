009cb1da46589c187f9e68fbf45c98f1d3be438c
McHaillet <martenchaillet@gmail.com>
2025-09-18 14:57:23 +0200

    remove at top level as well

--- src/miss_alignment/data/__init__.py
+++ src/miss_alignment/data/__init__.py
@@ -1,9 +1,7 @@
 from .training_datamodule import SHRECDataModule
-from .plot_data import plot_dataset
 from .download import download_training_data
 
 __all__ = [
     "SHRECDataModule",
-    "plot_dataset",
     "download_training_data",
 ]
