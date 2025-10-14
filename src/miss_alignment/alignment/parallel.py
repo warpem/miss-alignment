fixes for parallel running and changes in data units to ensure correct running on gpu's

src/miss_alignment/alignment/parallel.py
@@ -1,17 +1,23 @@
 import queue
 import multiprocessing as mp
 import time
+import torch
 from multiprocessing.managers import BaseProxy
 from pathlib import Path
 from typing import Optional
 
 from .tilt_series import evaluate_tilt_series
 
+try:
+    mp.set_start_method("spawn")
+except RuntimeError:
+    pass
+
 
 def gpu_runner(
-    device: str,
-    task_queue: BaseProxy,
-    result_queue: BaseProxy,
+        device: str,
+        task_queue: BaseProxy,
+        result_queue: BaseProxy,
 ) -> None:
     """Start a GPU runner, each runner should be initialized to a
     multiprocessing.Process() and manage running jobs on a single GPU. Each runner will
@@ -27,6 +33,7 @@ def gpu_runner(
     result_queue: mp.manager.BaseProxy
         shared queue from multiprocessing for finished jobs
     """
+    torch.set_num_threads(1)
     while True:
         try:
             task_parameters = task_queue.get_nowait()

src/miss_alignment/alignment/tilt_series.py
@@ -332,7 +332,7 @@ def evaluate_tilt_series(
 ) -> tuple[Tomogram, list[float]]:
     # load the best model and run alignment optimization
     model = MissAlignment.load_from_checkpoint(
-        model_checkpoint,
+        model_checkpoint, map_location='cpu',
     )
 
     # load tilt_series and set its name for output
@@ -354,10 +354,7 @@ def evaluate_tilt_series(
     # ensure we get a list of zyx positions!!
     position_grid = einops.rearrange(position_grid, 'd h w zyx -> (d h w) zyx')
 
-    # store initial shifts for reference
-    initial_translations = tilt_series.sample_translations.clone()
-
-    print(f"Aligning {tilt_series_name}...")
+    print(f"Aligning {tilt_series_name} on {device}")
     # return optimize_shifts_local(
     #     model,
     #     tilt_series,  # is modified in-place
@@ -373,13 +370,19 @@ def evaluate_tilt_series(
         patch_size,
         device,
     )
-    aligned_reconstruction = tilt_series.reconstruct_tomogram(
-        tomogram_shape, 128
-    )
 
     if tilt_series_ground_truth is not None:
         print(f"Centering alignment relative to ground truth...")
+        
+        tilt_series.to(device)
+        # store initial shifts for reference
+        initial_translations = tilt_series.sample_translations.clone()
+        aligned_reconstruction = tilt_series.reconstruct_tomogram(
+            tomogram_shape, 128
+        )
+
         tilt_series_ground_truth = read_tomogram_from_pickle(tilt_series_ground_truth)
+        tilt_series_ground_truth.to(device)
         ground_truth_alignment = tilt_series_ground_truth.sample_translations
         n_tilts, _, _ = tilt_series_ground_truth.images.shape
 
@@ -389,7 +392,7 @@ def evaluate_tilt_series(
 
         mean_diff_initial = torch.abs(
             ground_truth_alignment - initial_translations
-        )
+        ).cpu()
 
         correlation = calculate_cross_correlation(
             ground_truth_reconstruction, aligned_reconstruction
@@ -405,7 +408,7 @@ def evaluate_tilt_series(
 
         mean_diff_final = torch.abs(
             ground_truth_alignment - tilt_series.sample_translations
-        )
+        ).cpu()
 
         fig, ax = plt.subplots(1, 2)
         ax[0].plot(mean_diff_initial[:, 0], label="initial y")
@@ -423,7 +426,7 @@ def evaluate_tilt_series(
     #     voxel_size=10,
     #     overwrite=True,
     # )
-
+    tilt_series.to('cpu')
     save_tomogram_to_pickle(
         tilt_series, output_directory / f"{tilt_series_name}.pickle"
     )

src/miss_alignment/data/_augmentation.py
@@ -5,7 +5,8 @@ import torch
 def random_contrast(volume):
     """Slightly alter the mean and std of a pre-normalised tensor"""
     std_change, mean_change = (
-        torch.normal(1, 0.1, (1,)), torch.normal(0, 0.1, (1,))
+        torch.normal(1, 0.1, (1,), device=volume.device), 
+        torch.normal(0, 0.1, (1,), device=volume.device)
     )
     return volume * std_change + mean_change
 

src/miss_alignment/data/_reconstruction_worker.py
@@ -117,6 +117,7 @@ def reconstruction_worker(
         Device to use
     """
     torch.set_num_threads(1)
+
     print(
         f"Worker {worker_id} starting with indices {assigned_indices[:5]}..."
     )
@@ -226,12 +227,12 @@ def _create_pool_reconstruction(
     )
 
     # make tuple with volume and label
-    examples = [(aligned, 1), (misaligned, -1)]
+    examples = [(aligned.cpu(), 1), (misaligned.cpu(), -1)]
 
     # make a triplet example randomly mimick 1 or 2
-    examples += [(aligned.clone(), 1) if random.random() > 0.5 else (
-        misaligned.clone(), -1)]
-
+    examples += [(aligned.clone().cpu(), 1) if random.random() > 0.5 else (
+        misaligned.clone().cpu(), -1)]
+    
     return examples
 
 