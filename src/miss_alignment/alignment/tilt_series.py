d8c0e295f95fe2256cdc5e3aa74a2d7696e43236
McHaillet <martenchaillet@gmail.com>
2025-10-31 11:47:53 +0100

    move correlation code to separate file and refactor

--- src/miss_alignment/alignment/tilt_series.py
+++ src/miss_alignment/alignment/tilt_series.py
@@ -1,126 +1,45 @@
 from pathlib import Path
-from typing import Optional
 
 import math
 import functools
 import torch
 import einops
-import matplotlib.pyplot as plt
-from torch_tomogram import Tomogram
-from itertools import chain
 from warpylib.cubic_grid import CubicGrid
 from warpylib import TiltSeries
 
 from miss_alignment.data.io import TiltSeriesData
-from miss_alignment.data.shift_generation import project_shifts_3d_to_2d
 from miss_alignment.models import MissAlignment
 
 
-def calculate_cross_correlation(
-    a: torch.Tensor,
-    b: torch.Tensor,
+def generate_position_grid(
+        volume_dimensions_physical: tuple[float, float, float] | torch.Tensor,
+        pixel_size: float,
+        patch_size: int,
+        patch_overlap: float,
 ) -> torch.Tensor:
-    """
-    Calculate the 3D cross correlation between volumes of the same size.
+    # calculate patches per dimensions
+    stride = int(patch_size * (1 - patch_overlap))
+    patches_per_dim = []
+    for s in volume_dimensions_physical:
+        dim = s // pixel_size
+        patches_per_dim += [max(int(dim // stride), 1)]
 
-    The position of the maximum relative to the center of the volume gives a shift.
-    This is the shift that when applied to `b` best aligns it to `a`.
+    # extract patches per dim and physical size
+    px, py, pz = patches_per_dim
+    x, y, z = volume_dimensions_physical
 
-    Parameters
-    ----------
-    a : torch.Tensor
-        First 3D volume with shape (..., D, H, W)
-    b : torch.Tensor
-        Second 3D volume with shape (..., D, H, W)
+    # get the reconstruction positions to optimize over
+    zs = ((torch.arange(pz) + .5) / pz) * z
+    ys = ((torch.arange(py) + .5) / py) * y
+    xs = ((torch.arange(px) + .5) / px) * x
 
-    Returns
-    -------
-    torch.Tensor
-        3D cross-correlation volume
-    """
-    a = (a - a.mean()) / a.std()
-    b = (b - b.mean()) / b.std()
-    d, h, w = a.shape[-3:]
-    fta = torch.fft.rfftn(a, dim=(-3, -2, -1))
-    ftb = torch.fft.rfftn(b, dim=(-3, -2, -1))
-    result = fta * torch.conj(ftb)
-    result = torch.fft.irfftn(result, dim=(-3, -2, -1), s=(d, h, w))
-    result = torch.fft.ifftshift(result, dim=(-3, -2, -1))
-    result /= d * h * w  # normalize the result
-    return result
-
-
-def get_shift_from_correlation_image(correlation_image: torch.Tensor) -> torch.Tensor:
-    """
-    Extract shift from 3D correlation volume.
-
-    The shift should be applied to img2 to align with img1.
-    Uses parabolic interpolation for sub-voxel accuracy.
-
-    Parameters
-    ----------
-    correlation_image : torch.Tensor
-        3D correlation volume
-
-    Returns
-    -------
-    torch.Tensor
-        3D shift vector [z, y, x]
-    """
-    d, h, w = correlation_image.shape
-    dtype, device = correlation_image.dtype, correlation_image.device
-
-    image_shape = torch.as_tensor(correlation_image.shape, device=device, dtype=dtype)
-    center = torch.divide(image_shape, 2, rounding_mode="floor")
-
-    # Find peak location
-    flat_idx = torch.argmax(correlation_image)
-    peak_z = flat_idx // (h * w)
-    peak_y = (flat_idx % (h * w)) // w
-    peak_x = flat_idx % w
-
-    # Check if peak is on border
-    if (
-        peak_z == 0
-        or peak_z == d - 1
-        or peak_y == 0
-        or peak_y == h - 1
-        or peak_x == 0
-        or peak_x == w - 1
-    ):
-        shift = (
-            torch.tensor([peak_z, peak_y, peak_x], device=device, dtype=dtype) - center
-        )
-        return shift
-
-    # Parabolic interpolation in z direction
-    f_z0 = correlation_image[peak_z - 1, peak_y, peak_x]
-    f_z1 = correlation_image[peak_z, peak_y, peak_x]
-    f_z2 = correlation_image[peak_z + 1, peak_y, peak_x]
-    subpixel_peak_z = peak_z + 0.5 * (f_z0 - f_z2) / (f_z0 - 2 * f_z1 + f_z2)
-
-    # Parabolic interpolation in y direction
-    f_y0 = correlation_image[peak_z, peak_y - 1, peak_x]
-    f_y1 = correlation_image[peak_z, peak_y, peak_x]
-    f_y2 = correlation_image[peak_z, peak_y + 1, peak_x]
-    subpixel_peak_y = peak_y + 0.5 * (f_y0 - f_y2) / (f_y0 - 2 * f_y1 + f_y2)
-
-    # Parabolic interpolation in x direction
-    f_x0 = correlation_image[peak_z, peak_y, peak_x - 1]
-    f_x1 = correlation_image[peak_z, peak_y, peak_x]
-    f_x2 = correlation_image[peak_z, peak_y, peak_x + 1]
-    subpixel_peak_x = peak_x + 0.5 * (f_x0 - f_x2) / (f_x0 - 2 * f_x1 + f_x2)
-
-    subpixel_shift = (
-        torch.tensor(
-            [subpixel_peak_z, subpixel_peak_y, subpixel_peak_x],
-            device=device,
-            dtype=dtype,
-        )
-        - center
+    # merge positions in tensor
+    position_grid = (
+        torch.stack(torch.meshgrid(xs, ys, zs, indexing='ij'), dim=-1)
     )
-
-    return subpixel_shift
+    # ensure we get a list of xyz positions!!
+    position_grid = einops.rearrange(position_grid, 'd h w xyz -> (d h w) xyz')
+    return position_grid
 
 
 def optimize_shifts(
@@ -178,7 +97,7 @@ def optimize_shifts(
             device=device,
         )
         parameters = [shifts_y, shifts_x]
-    elif len(setting) == 3:
+    elif len(setting) == 3:  #TODO add case of starting from existent grid
         # movement grids - these should receive gradients
         n_params = int(functools.reduce(lambda x, y: x * y, setting))
         movement_x_leaf = (
@@ -190,7 +109,7 @@ def optimize_shifts(
         )
         tilt_series.grid_movement_y = CubicGrid(setting, movement_y_leaf)
         parameters = [movement_x_leaf, movement_y_leaf]
-    elif len(setting) == 4:
+    elif len(setting) == 4:  #TODO add case of starting from existent grid
         n_params = int(functools.reduce(lambda x, y: x * y, setting))
         movement_x_leaf = (
             torch.zeros(n_params, requires_grad=True, device=device)
@@ -305,15 +224,14 @@ def optimize_shifts(
 def evaluate_tilt_series(
         model_checkpoint_path: Path,
         tilt_series_path: Path,
-        patches_per_dim: tuple[int, int, int],
         output_directory: Path,
         setting: str |
                  tuple[int, int, int] |
                  tuple[int, int, int, int] = 'global',
         patch_size: int = 96,
+        patch_overlap: float = 0.1,
         batch_size: int = 16,
         apply_ctf: bool = True,
-        ground_truth_path: Optional[Path] = None,
         device: str = "cpu",
 ) -> tuple[Path, list[float]]:
     # load the best model and run alignment optimization
@@ -326,23 +244,15 @@ def evaluate_tilt_series(
     tilt_series_data = TiltSeriesData.from_json(tilt_series_path)
     tilt_series, images, pixel_size = tilt_series_data.load_metadata_and_stack()
 
-    x, y, z = tilt_series.volume_dimensions_physical
-    px, py, pz = patches_per_dim
-
-    # get the reconstruction positions to optimize over
-    zs = ((torch.arange(pz) + .5) / pz) * z
-    ys = ((torch.arange(py) + .5) / py) * y
-    xs = ((torch.arange(px) + .5) / px) * x
-
-    position_grid = (
-        torch.stack(torch.meshgrid(xs, ys, zs, indexing='ij'), dim=-1)
+    # generate a position grid to optimize over
+    position_grid = generate_position_grid(
+        volume_dimensions_physical=tilt_series.volume_dimensions_physical,
+        pixel_size=pixel_size,
+        patch_size=patch_size,
+        patch_overlap=patch_overlap,
     )
-    # ensure we get a list of xyz positions!!
-    position_grid = einops.rearrange(position_grid, 'd h w xyz -> (d h w) xyz')
-    
-    # store initial translations, before tilt-series is modified in-place
-    #initial_translations = tilt_series.sample_translations.clone()
 
+    # run alignment optimization with the model
     tilt_series, loss = optimize_shifts(
         model,
         tilt_series,
@@ -356,55 +266,7 @@ def evaluate_tilt_series(
         device=device,
     )
 
-    if ground_truth_path is not None:
-        print(f"Centering alignment relative to ground truth...")
-        
-        # move back to device for faster reconstruction/cross-correlation
-        tilt_series.to(device)
-        initial_translations = initial_translations.to(device)
-        aligned_reconstruction = tilt_series.reconstruct_tomogram(
-            tomogram_shape, 128
-        )
-
-        tilt_series_ground_truth = read_tomogram_from_pickle(ground_truth_path)
-        tilt_series_ground_truth.to(device)
-        ground_truth_alignment = tilt_series_ground_truth.sample_translations
-        n_tilts, _, _ = tilt_series_ground_truth.images.shape
-
-        ground_truth_reconstruction = tilt_series_ground_truth.reconstruct_tomogram(
-            tomogram_shape, 128
-        )
-
-        mean_diff_initial = torch.abs(
-            ground_truth_alignment - initial_translations
-        ).cpu()
-
-        correlation = calculate_cross_correlation(
-            ground_truth_reconstruction, aligned_reconstruction
-        )
-        shift_3d = get_shift_from_correlation_image(correlation)
-        shift_3d = -1 * shift_3d  # get the forward shift for the imaging model
-        shift_3d = shift_3d.repeat(n_tilts, 1)
-
-        shifts_2d = project_shifts_3d_to_2d(
-            shift_3d, tilt_series.projection_matrices[..., 1:3, :3]
-        )
-        tilt_series.sample_translations += shifts_2d
-
-        mean_diff_final = torch.abs(
-            ground_truth_alignment - tilt_series.sample_translations
-        ).cpu()
-
-        fig, ax = plt.subplots(1, 2)
-        ax[0].plot(mean_diff_initial[:, 0], label="initial y")
-        ax[0].plot(mean_diff_final[:, 0], label="final y")
-        ax[1].plot(mean_diff_initial[:, 1], label="initial x")
-        ax[1].plot(mean_diff_final[:, 1], label="final x")
-        ax[0].legend()
-        ax[1].legend()
-
-        plt.savefig(output_directory / f"{tilt_series_name}_yx_diff_per_tilt.png")
-
+    # write all necessary output
     xml_out = output_directory / (tilt_series_data.xml_filename + '.xml')
     json_out = output_directory / (tilt_series_data.xml_filename + '.json')
     new_tilt_series_data = (
