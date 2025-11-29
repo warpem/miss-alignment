0f2685552298043751a34bbcf36860a74a045f20
Dimitry Tegunov <tegunovd@gene.com>
2025-11-29 17:07:56 +0000

    change default model to do more before first downsampling

--- src/miss_alignment/models/_compact.py
+++ src/miss_alignment/models/_compact.py
@@ -7,33 +7,46 @@ class Compact3DConvNet(nn.Module):
 
         # Feature extraction with progressive downsampling
         self.conv = nn.Sequential(
-            # Layer 1: 64x64x64 -> 32x32x32
-            nn.Conv3d(1, 8, kernel_size=3, stride=2, padding=1),
+            # Layer 0: 64x64x64 -> 64x64x64
+            nn.Conv3d(1, 8, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm3d(8),
-            nn.ReLU(),  # something else? ELU might be worth to test
-            # Layer 2: 32x32x32 -> 16x16x16
+            nn.SiLU(),  # something else? ELU might be worth to test
+            # Layer 1: 64x64x64 -> 32x32x32
             nn.Conv3d(8, 16, kernel_size=3, stride=2, padding=1),
             nn.BatchNorm3d(16),
-            nn.ReLU(),
-            # Layer 3: 16x16x16 -> 8x8x8
+            nn.SiLU(),  # something else? ELU might be worth to test
+            # Layer 2: 32x32x32 -> 16x16x16
             nn.Conv3d(16, 32, kernel_size=3, stride=2, padding=1),
             nn.BatchNorm3d(32),
-            nn.ReLU(),
+            nn.SiLU(),
+            # Layer 3: 16x16x16 -> 8x8x8
+            nn.Conv3d(32, 64, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm3d(64),
+            nn.SiLU(),
             # Layer 4: 8x8x8 -> 4x4x4
-            nn.Conv3d(32, 32, kernel_size=3, stride=2, padding=1),
-            nn.BatchNorm3d(32),
-            nn.ReLU(),
-            # Global average pooling: 4x4x4 -> 1x1x1
+            nn.Conv3d(64, 64, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm3d(64),
+            nn.SiLU(),
+            # Layer 5: 4x4x4 -> 2x2x2
+            nn.Conv3d(64, 64, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm3d(64),
+            nn.SiLU(),
+            # Global average pooling: 2x2x2 -> 1x1x1
             nn.AdaptiveAvgPool3d(1),
         )
 
         # Final regression layer
-        self.regressor = nn.Linear(32, 1)
+        self.regressor = nn.Sequential(
+            nn.Linear(64, 16),
+            nn.BatchNorm1d(16),
+            nn.SiLU(),
+            nn.Linear(16, 1, bias=False)
+        )
 
     def forward(self, x):
         # Assuming input shape: (batch_size, 1, 64, 64, 64)
         x = self.conv(x)
-        x = x.view(x.size(0), -1)  # Flatten: (batch_size, 32)
+        x = x.view(x.size(0), -1)  # Flatten: (batch_size, 64)
         x = self.regressor(x)  # Final output: (batch_size, 1)
         return x
 
